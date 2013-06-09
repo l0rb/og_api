228668abced120fe6821c6a4ea8456fb6c7cd02c
Carl Schönbach <carl.schoenbach@gmail.com>
2013-06-09 17:49:02 +0200

    make irc_handler executable from commandline for better testing

--- irc_handler.py
+++ irc_handler.py
@@ -6,16 +6,16 @@ server = "uni117.ogame.de"
 api = api.Api(server, "var", quick=True)
 
 def handle_command(connection, e, command):
-    #    try:
-    #        do_logging(connection, e, command)
-    #    except:
-    #        pass
+    target = e.target
+    if e.type == "privmsg" or command.startswith("help"):
+        target = e.source.nick
+
     try:
         if command[0] != '!':
             return
         command = command[1:]
         print command
-        realy_handle_command(connection, e, command)
+        retStr = realy_handle_command(command, connection=connection, target=target)
     except Exception, ex:
         target = e.target
         if e.type == "privmsg":
@@ -27,13 +27,11 @@ def handle_command(connection, e, command):
             pass
         print "error ", ex
 
-def realy_handle_command(connection, e, command):
-    #nick = e.source.nick
-    #channel = e.target
-    #e.type == pubmsg
-    target = e.target
-    if e.type == "privmsg":
-        target = e.source.nick
+    for line in retStr:
+        connection.privmsg(target, line)
+
+def realy_handle_command(command, connection=False, target=False):
+    retStr = []
     s = re.search("^(?P<search>(a|f)?)(?P<type>(player)|(alliance)) (?P<name>.+)$", command)
     if s:
         if s.group("search") == "a":
@@ -52,20 +50,19 @@ def realy_handle_command(connection, e, command):
                 data = api.findAlliance(name, 6)
             else:
                 data = api.getAllianceString(name)
-        for line in "".join(data).split("\n"):
-            connection.privmsg(target, line)
+        retStr = "".join(data).split("\n")
+
     elif command == "help":
-        target = e.source.nick # help commands should only be private
-        connection.privmsg(target, u"!player name - infos zu einem spieler")
-        connection.privmsg(target, u"!alliance tag - infos zu einer allianz")
-        connection.privmsg(target, u"!aplayer bzw. aalliance - gibt erweiterte infos")
-        connection.privmsg(target, u"!fplayer bzw. falliance - gibt 6 spieler/allianz nach Ähnlichkeit zurück")
-        connection.privmsg(target, u"!which met kris deut [plasma] [temp] [kurs] - gibt Vorschlag welche Mine am besten zu bauen ist - help which für mehr")
-        connection.privmsg(target, u"!awhich same as which, but a bit more detailed")
+        retStr.append(u"!player name - infos zu einem spieler")
+        retStr.append(u"!alliance tag - infos zu einer allianz")
+        retStr.append(u"!aplayer bzw. aalliance - gibt erweiterte infos")
+        retStr.append(u"!fplayer bzw. falliance - gibt 6 spieler/allianz nach Ähnlichkeit zurück")
+        retStr.append(u"!which met kris deut [plasma] [temp] [kurs] - gibt Vorschlag welche Mine am besten zu bauen ist - help which für mehr")
+        retStr.append(u"!awhich same as which, but a bit more detailed")
+
     elif command == "help which":
-        target = e.source.nick # help commands should only be private
-        connection.privmsg(target, u"Beispielaufruf: !which 18 15 12 für met=18,kris=15 und deut=12 - plasmatech=0 und maximale temperatur=50 da nicht angegeben, kurs=2:1:1")
-        connection.privmsg(target, u"Beispielaufruf: !which 18 15 12 3 45 3:2:1 nun ist plasma=3, temp=45 und der kurs 3:2:1")
+        retStr.append(u"Beispielaufruf: !which 18 15 12 für met=18,kris=15 und deut=12 - plasmatech=0 und maximale temperatur=50 da nicht angegeben, kurs=2:1:1")
+        retStr.append(u"Beispielaufruf: !which 18 15 12 3 45 3:2:1 nun ist plasma=3, temp=45 und der kurs 3:2:1")
 
     elif command.startswith("which ") or command.startswith("awhich"):
         args = command.split(" ")
@@ -87,11 +84,9 @@ def realy_handle_command(connection, e, command):
             for i in args[6].split(":"):
                 mse.append(float(i))
         import Constants
-        reload(Constants)
         if command.startswith("which "):
             bId = Constants.buildingTopList(buildings, {122:plasma}, temp, mse)[0]["bId"]
-            msg = "Am besten baust du: %s" % Constants.buildLabels[bId]
-            connection.privmsg(target, msg)
+            retStr.append("Am besten baust du: %s" % Constants.buildLabels[bId])
         else:
             t = Constants.buildingTopList(buildings, {122:plasma}, temp, mse)
             i = 0
@@ -101,13 +96,15 @@ def realy_handle_command(connection, e, command):
                 atime = build["atime"]
                 import datetime
                 atime = str(datetime.timedelta(seconds=atime))
-                msg = "%d: %s %s" % (i, Constants.buildLabels[bId], atime)
-                connection.privmsg(target, msg)
+                retStr.append("%d: %s %s" % (i, Constants.buildLabels[bId], atime))
+
     elif command == "dbupdate":
         import db
-        connection.privmsg(target, "updating db - this takes some time..")
+        if connection:
+            connection.privmsg(target, "updating db - this takes some time..")
         db.update(server)
-        connection.privmsg(target, "updated")
+        retStr.append("updated")
+
     elif command.startswith("dbquery"):
         import db
         try:
@@ -115,15 +112,11 @@ def realy_handle_command(connection, e, command):
         except Exception, ex:
             connection.privmsg(target, ex)
             if len(command[8:]) == 445:
-                connection.privmsg(target, "Your query can't be longer than 445 chars")
-                connection.privmsg(target, command[8:])
+                retStr.append("Your query can't be longer than 445 chars")
+                retStr.append(command[8:])
             return
-        max = 10
-        for line in res:
-            connection.privmsg(target, line)
-            max -= 1
-            if max == 0:
-                break
+        retStr = res[:9]
+
     elif command.startswith("inactive"):
         args = command[9:].split(" ")
         import db
@@ -132,24 +125,34 @@ def realy_handle_command(connection, e, command):
         minScore = 5000
         maxScore = 9999999
         if len(args) < 1 or args[0].find(":") == -1:
-            connection.privmsg(target, "usage !inactive yourpos [radius] [duration] [minscore] [maxscore]")
-            connection.privmsg(target, "example !inactive 3:338 15 2 5000 20000 - sucht alle planeten zwischen 3:323 und 3:353 wo spieler 2Tage inaktive und zw. 5k und 20k punkte hat")
-            return
-        if len(args) > 0:
-             position = args[0]
-        if len(args) > 1:
-            radius = int(args[1])
-        if len(args) > 2:
-             duration = int(args[2]) * 60*60*24
-        if len(args) > 3:
-            minScore = int(args[3])
-        if len(args) > 4:
-            maxScore = int(args[4])
+            retStr.append("usage !inactive yourpos [radius] [duration] [minscore] [maxscore]")
+            retStr.append("example !inactive 3:338 15 2 5000 20000 - sucht alle planeten zwischen 3:323 und 3:353 wo spieler 2Tage inaktive und zw. 5k und 20k punkte hat")
+        else:
+            if len(args) > 0:
+                 position = args[0]
+            if len(args) > 1:
+                radius = int(args[1])
+            if len(args) > 2:
+                 duration = int(args[2]) * 60*60*24
+            if len(args) > 3:
+                minScore = int(args[3])
+            if len(args) > 4:
+                maxScore = int(args[4])
+            res = "".join(db.listInactivityPlayer(position, radius, duration, minScore, maxScore)).split("\n")
+            retStr = res[:5]
 
-        max = 5
-        res = "".join(db.listInactivityPlayer(position, radius, duration, minScore, maxScore)).split("\n")
-        for line in res:
-            connection.privmsg(target, line)
-            max -= 1
-            if max == 0:
-                break
+    return retStr
+
+
+if __name__ == "__main__":
+    def Args():
+        pass
+    args = Args()
+
+    import argparse
+    parser = argparse.ArgumentParser(description='get info from ogame api db',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--command', '-c', type=str, help='command to execute')
+    args = parser.parse_args()
+
+    print "\n".join(realy_handle_command(args.command))
